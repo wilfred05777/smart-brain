bfbd914b8b1c7447ef90a77700239bf986a47895
Wilfred Erdo <wilfred05777@gmail.com>
2019-02-11 10:15:06 +0800

    adding cors to json

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('https://face-recognition-erdo.herokuapp.com/imageurl', {
+    fetch('https://face-recognition-wilfred.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -85,7 +85,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://face-recognition-erdo.herokuapp.com/image', {
+          fetch('https://face-recognition-wilfred.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
